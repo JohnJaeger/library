a3967abdcfa6c625bd888cf09f776371fa9b0a4a
JohnJaeger <jmjaeger10@gmail.com>
2024-10-19 20:20:26 -0400

    Change Book constructor to include author parameter
    - Book constructor was edited to include author parameter.
    - Constructor function will throw an error if an incorrect author
    argument is given.

--- javascript.js
+++ javascript.js
@@ -5,12 +5,15 @@ myLibrary.push(new Book("Moby-Dick", 635, false));
 myLibrary.push(new Book("1984", 368, true));
 myLibrary.push(new Book("The Odyssey", 416, false));
 
-function Book(name, pages, haveRead){
+function Book(name, author, pages, haveRead){
     if (!(new.target)){
         throw new Error('function must be used with new keyword!');
     } else {
         if (typeof name !== 'string') {
             throw new Error('name must be a string!');
+        }
+        if (typeof author !== 'string') {
+            throw new Error('author must be a string!');
         }
         if (typeof pages !== 'number') {
             throw new Error('pages must be a number!');
@@ -19,11 +22,8 @@ function Book(name, pages, haveRead){
             throw new Error('haveRead must be a boolean!');
         }
         this.name = name;
+        this.author = author;
         this.pages = pages;
         this.haveRead = haveRead;
-        this.info = function(){
-            haveRead === true ? console.log(`${name}, ${pages} long, user has read book`)
-                            : console.log(`${name}, ${pages} long, user has not read book`);
-        }
     }
 }
