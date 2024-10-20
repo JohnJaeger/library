77547e4dc7a15d85571a782728e75d8214e7bf83
JohnJaeger <jmjaeger10@gmail.com>
2024-10-20 14:21:40 -0400

    Add updateLibrary method
    - library.updateLibrary() will replace book html elements with
    ones that are up to date according to library.books array.
    - The book elements are created with the proper classes, attributes,
    and text for each specific book in the library.books array.

--- javascript.js
+++ javascript.js
@@ -14,6 +14,57 @@ const library = {
             throw new Error('haveRead must be a boolean!');
         }
         this.books.push(new Book(title, author, pages, haveRead));
+    },
+    updateLibrary: function(){
+        const main = document.querySelector('main');
+        const addBook = document.querySelector('.add-book');
+    
+        //Remove book html elements
+        Array.from(main.children)
+            .filter(child => child !== addBook)
+            .forEach(child => child.remove());
+
+        //Iterate through books and add book html elements
+        for (const object of this.books){
+            const book = document.createElement('div');
+            const bookCover = document.createElement('img');
+            const bookInformation = document.createElement('div');
+            const bookTitle = document.createElement('span');
+            const bookAuthor = document.createElement('span');
+            const bookPages = document.createElement('span');
+            const bookReadIndicator = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    
+            book.classList.add('book');
+            bookCover.classList.add('book-cover');
+            bookInformation.classList.add('book-information');
+            bookTitle.classList.add('book-title');
+            bookAuthor.classList.add('book-author');
+            bookPages.classList.add('book-pages');
+            bookReadIndicator.classList.add('book-read-indicator');
+    
+            if (object.haveRead === true){
+                bookReadIndicator.classList.add('read');
+            }
+    
+            bookCover.setAttribute('src', 'SVG/book-cover.svg');
+            bookCover.setAttribute('alt', 'Default book cover');
+            bookReadIndicator.setAttribute('viewBox', '0 0 24 24');
+            path.setAttribute('d', 'M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z');
+    
+            bookTitle.textContent = object.title;
+            bookAuthor.textContent = object.author;
+            bookPages.textContent = object.pages + " Pages";
+    
+            bookReadIndicator.appendChild(path);
+            bookInformation.appendChild(bookTitle);
+            bookInformation.appendChild(bookAuthor);
+            bookInformation.appendChild(bookPages);
+            bookInformation.appendChild(bookReadIndicator);
+            book.appendChild(bookCover);
+            book.appendChild(bookInformation);
+            main.insertBefore(book, addBook);
+        }
     }
 };
 
